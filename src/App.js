10ed556bd115dd9d66711f9671f03e901c33731c
newshopstyle <1themotiv@gmail.com>
2024-09-19 01:09:03 -0700

    Fix ESLint import order and add missing Babel plugin

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
+// backend/frontend/src/App.js
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Store from './pages/Store';
-import Navbar from './components/Navbar';
-import './App.css';
-import PrivateRoute from './components/PrivateRoute';
-
-
-
-
-// Внутри <Routes>
-<Route
-  path="/store/:id"
-  element={
-    <PrivateRoute>
-      <Store />
-    </PrivateRoute>
-  }
-/>
 import CreateStore from './pages/CreateStore';
-
-// Внутри <Routes>
-<Route
-  path="/create-store"
-  element={
-    <PrivateRoute>
-      <CreateStore />
-    </PrivateRoute>
-  }
-/>
 import AddProduct from './pages/AddProduct';
+import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
+import './App.css';
 
-// Внутри <Routes>
-<Route
-  path="/add-product"
-  element={
-    <PrivateRoute>
-      <AddProduct />
-    </PrivateRoute>
-  }
-/>
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        {/* Публичные маршруты */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/store/:id" element={<Store />} />
+
+        {/* Приватные маршруты */}
+        <Route
+          path="/store/:id"
+          element={
+            <PrivateRoute>
+              <Store />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/create-store"
+          element={
+            <PrivateRoute>
+              <CreateStore />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/add-product"
+          element={
+            <PrivateRoute>
+              <AddProduct />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
